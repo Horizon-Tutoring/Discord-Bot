12005bfaf9f8a3967204a5e3123fd5787f3d9a5b
JoshuaMicallefYBSU <joshuamicallef66@gmail.com>
2023-07-17 18:11:32 +1000

    Update to server code

---
+++ api/APIController.php
@@ -0,0 +1,59 @@
+<?php
+
+use Discord\Discord;
+use Discord\Parts\Channel\Message;
+use Discord\WebSockets\Intents;
+use Discord\WebSockets\Event;
+use Discord\Parts\User\Activity;
+
+    // Check all messages
+    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+
+        // CHANNELS ALLOWED
+        $allowedChannels = [
+        '1045223017518743602', //devops
+        '1037188637000994899', //general
+        '1125445580794110013' //bot-requests
+        ];
+
+        // Check if the message is from an allowed channel
+        if (!in_array($message->channel_id, $allowedChannels)) {
+            return;
+        }
+
+        // Ignore Bot Messages
+        if ($message->author->id == $discord->user->id) {
+            return;
+        }
+
+        // Ping Bot Code
+        if ($message->content == '!ping') {
+            $message->reply('pong!')->done(function (Message $message) {
+                // Schedule the deletion after 1 minute
+                $discord->loop->addTimer(60, function () use ($message) {
+                    $message->delete();
+                });
+            });
+            return;
+        }
+
+
+        // Shutdown Bot Code
+        if ($message->content == '!shutdown' && $message->author->id == '200426385863344129') {
+            $message->reply('Understood Joshua, Shutting down...')->done(function (Message $message) use ($discord) {
+
+                $activity = $discord->factory(\Discord\Parts\User\Activity::class, [
+                    'name' => 'Shutting Down Simulator',
+                    'type' => \Discord\Parts\User\Activity::TYPE_LISTENING,
+                ]);
+
+                $discord->updatePresence(null, false, 'invisible', false);
+                
+                $discord->close();
+            });
+            return;
+        }
+
+        
+
+    });
--- bot.php
+++ bot.php
@@ -1,8 +1,29 @@
 <?php
 
-include __DIR__.'/vendor/autoload.php';
+$lockFile = __DIR__.'/script.lock';
 
-set_time_limit(60);
+// Check if lock file exists
+if (file_exists($lockFile)) {
+
+    // Check if the script is still running
+    $pid = intval(file_get_contents($lockFile));
+    if (posix_kill($pid, 0)) {
+        // Another instance is running, exit the current script
+        echo "The Horizon Tutoring Bot is still running on the server. Exiting restart script.\n";
+        exit;
+
+    } else {
+        // The lock file exists, but the previous script has terminated. Remove the lock file.
+        unlink($lockFile);
+    }
+}
+
+// Create the lock file
+file_put_contents($lockFile, getmypid());
+
+echo "[SYSTEM]\nThe bot is not currently running. The service is now being started! \n \n";
+
+include __DIR__.'/vendor/autoload.php';
 
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
@@ -20,7 +41,13 @@ $discord = new Discord([
 ]);
 
 $discord->on('ready', function (Discord $discord) {
-    echo "Bot is ready!", PHP_EOL;
+    echo "[SYSTEM]\nThe bot is now operating. Dispensing Activation Message in #server-logs \n \n", PHP_EOL;
+
+    // Find the channel by its ID
+    $channel = $discord->getChannel(1096629948363571291);
+    
+    // Send the message in the channel
+    $channel->sendMessage(date('[Y-m-d H:i:s]') . 'The Horizon Tutoring Bot has now been started');
 
     $activity = $discord->factory(\Discord\Parts\User\Activity::class, [
         'name' => '!shutdown',
@@ -33,3 +60,6 @@ $discord->on('ready', function (Discord $discord) {
 });
 
 $discord->run();
+
+// Remove the lock file before exiting
+unlink($lockFile);
